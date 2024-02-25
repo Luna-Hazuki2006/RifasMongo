3671923369a8b8193747382b4554c10a3d89c8cb
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-02-25 13:50:40 -0400

    aaaaaaaaa

--- api/config/coneccion.js
+++ api/config/coneccion.js
@@ -1,5 +1,5 @@
 import { MongoClient } from "mongodb";
-
-const mongo = new MongoClient("mongodb+srv://lunahazuki2006:cXU0lYhSncWZ12F@cluster0.owjghpf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+// mongodb+srv://lunahazuki2006:@cluster0.owjghpf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+const mongo = new MongoClient("")
 
 export {mongo}
